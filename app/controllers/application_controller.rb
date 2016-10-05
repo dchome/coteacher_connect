c48c03b54ae0e8df0961014c3b571f114335fb95
Daniel Homer <daniel.homer6@gmail.com>
2016-10-05 14:05:12 -0400

    add turbolinks back to support ajax calls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session,
+      if: Proc.new { |c| c.request.format =~ %r{application/json} }
+
+  def current_teacher
+    Teacher.find_by(id: session[:teacher_id])
+  end
+
+  def logged_in?
+    session[:teacher_id] && current_teacher.present?
+  end
 end
--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -3,7 +3,34 @@ class Classroom < ActiveRecord::Base
   has_many :students
   has_many :notes, through: :students
   has_many :components, through: :students
-  has_many :messages
+  has_many :messages, dependent: :destroy
 
   validates :name, :schoolyear, presence: true
+
+  def list_teacher_names
+    teachers = self.teachers
+    if teachers != []
+      list = teachers[0].full_name
+      list += ", " + teachers[1].full_name if(teachers[1])
+    else
+      list = "Unassigned"
+    end
+    list
+  end
+
+  #gets most recent messages in sets of ten, where set_idx of 0 is 1st to 10th most recent, set_idx of 1 is 11th to 20th most recent, etc. Gets all messages, or all within the specified set, if the set has fewer than 10 messages in it.
+  def ten_messages(set_idx)
+    set_idx = set_idx || 0
+    idx1 = -10 + (-10* set_idx)
+    idx2 = -1 + (-10*set_idx)
+    if !self.messages[idx2]
+      return nil
+    else
+      while !self.messages[idx1]
+        idx1 += 1
+      end
+      return self.messages[idx1..idx2]
+    end
+  end
+
 end
